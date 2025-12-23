cf8383d9b263d90e1aeea0f2ddb91d68e01de053
Hoanglucky <duongminhhoang2008@gmail.com>
2025-12-23 18:10:44 +0700

    video23

---
+++ dist/app.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = 8080;
+app.get("/", (req, res) => {
+    res.send("Test server");
+});
+app.get("/admin", (req, res) => {
+    res.send("Test admin");
+});
+app.listen(PORT, () => {
+    console.log('My app is running on port : $(PORT)');
+});
+//# sourceMappingURL=app.js.map
